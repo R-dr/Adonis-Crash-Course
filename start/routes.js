9e03d26502780593aa28f7e72c8aeae820ff0725
Rodney <rodney.ditch@hotmail.com>
2021-03-15 18:08:29 +1100

    adonis params test in routes

--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,8 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use("Route");
 
-Route.on('/').render('welcome')
+Route.on("/").render("home");
+Route.get("/test", () => "hello World");
+Route.get("/test/:id", ({ params }) => `this is test id: ${params.id}`);
