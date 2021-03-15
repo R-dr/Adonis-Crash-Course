cf07e893d58561e0a178280b1d1328c3a6ac36ac
Rodney <rodney.ditch@hotmail.com>
2021-03-15 18:20:29 +1100

    how to loop through arrays, for objects use for in term in view

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,10 +1,30 @@
 "use strict";
 
 class PostController {
+  // hardcoded posts data, will hook up MySQL later
+
   //index method
   async index({ view }) {
+    const posts = [
+      {
+        title: "Post One",
+        body: "post one body",
+      },
+      {
+        body: "post Two body",
+      },
+      {
+        title: "Post Three",
+        body: "post three body",
+      },
+      {
+        title: "Post Four",
+        body: "post Four body",
+      },
+    ];
     return view.render("posts/index", {
-      title: "latest Posts",
+      title: "Latest Posts",
+      posts,
     });
   }
 }
