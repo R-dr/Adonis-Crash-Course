30700a40f709ff374df37002ef888a0b13d4a367
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-03-15 22:48:44 +1100

    Switched to linux machine, got ySQL working and have posts cmoing through from database

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,30 +1,34 @@
 "use strict";
 
+// Post model
+const Post = use('App/Models/Post')
+
 class PostController {
   // hardcoded posts data, will hook up MySQL later
 
   //index method
   async index({ view }) {
-    const posts = [
-      {
-        title: "Post One",
-        body: "post one body",
-      },
-      {
-        body: "post Two body",
-      },
-      {
-        title: "Post Three",
-        body: "post three body",
-      },
-      {
-        title: "Post Four",
-        body: "post Four body",
-      },
-    ];
+    // const posts = [
+    //   {
+    //     title: "Post One",
+    //     body: "post one body",
+    //   },
+    //   {
+    //     body: "post Two body",
+    //   },
+    //   {
+    //     title: "Post Three",
+    //     body: "post three body",
+    //   },
+    //   {
+    //     title: "Post Four",
+    //     body: "post Four body",
+    //   },
+    // ];
+    const posts = await Post.all()
     return view.render("posts/index", {
       title: "Latest Posts",
-      posts,
+      posts:posts.toJSON(),
     });
   }
 }
